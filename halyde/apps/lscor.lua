66783e455cbc18facb0ce216f839ac1e6d26aea3
Ponali <ponali2k@gmail.com>
2025-09-15 07:08:19 +0200

    ported apps lscor, mv, and rm
    there's partially rtest too, if you count that

--- halyde/apps/lscor.lua
+++ halyde/apps/lscor.lua
@@ -4,5 +4,5 @@ for i=1, #tasks do
     local pipeChar = "├ "
     if i==#tasks then pipeChar = "└ " end
     local task = tasks[i]
-    print("\27[93m"..pipeChar..i.."\27[0m - "..task.name.."\27[37m "..table.concat(task.args or {}," ").." \27[0m")
+    print("\27[93m"..pipeChar..(task.id or i).."\27[0m - "..task.name.."\27[37m "..table.concat(task.args or {}," ").." \27[0m")
 end
--- halyde/apps/rm.lua
+++ halyde/apps/rm.lua
@@ -1,4 +1,5 @@
 local file = ...
+local shell = require("shell")
 local fs = require("filesystem")
 
 if not file then
@@ -6,7 +7,7 @@ if not file then
   return
 end
 if file:sub(1, 1) ~= "/" then
-  file = fs.concat(shell.workingDirectory, file)
+  file = fs.concat(shell.getWorkingDirectory(), file)
 end
 if not fs.exists(file) then
   print("\27[91mFile does not exist.")
