2c754288e83698069a4ba201308eeb116e6b3ad9
WahPlus <wahplus@proton.me>
2026-04-30 18:09:31 +0300

    But did YOU know

--- .github/workflows/minify.js
+++ .github/workflows/minify.js
@@ -11,4 +11,4 @@ const Settings = {
 
 const Beautified = luamin.Beautify(Code, Settings)
 const Minified = luamin.Minify(Code, Settings)
-fs.writeFileSync(process.argv[2],"utf-8")
+fs.writeFileSync(process.argv[2],Minified)
